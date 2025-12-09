5768f5830b5a7a7ad1e8bb5c113884e073709597
xcneonz <abdulrahman2004an@gmail.com>
2025-12-09 18:07:47 +0300

    first stage of data cleaning, filtered the imp columns and removes all na's from description column

--- src/main.py
+++ src/main.py
@@ -3,3 +3,13 @@ import pandas as pd
 data = pd.read_csv("fake_job_postings.csv")
 print(data.head())
 
+#Data Cleaning
+
+data = data[['description', 'fraudulent']]
+
+data = data.dropna(subset = ['description'])
+
+data = data.reset_index(drop = True) # - reset index
+
+print(f"number of sample data after cleaning {len(data)}")
+print(data.head())
